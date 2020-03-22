a0d87b735925257521b328d9c4d4081c06fd94b1
rao xun <raoxunjob@163.com>
2020-03-23 01:23:58 +0800

    add uv

---
+++ CPP/bin/CMakeFiles/uv_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uv_main.dir/uv.cpp.o"
+  "uv_main.pdb"
+  "uv_main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uv_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
